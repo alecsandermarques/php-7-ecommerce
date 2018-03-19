352c1504cd4e6ce2faa82267cd24828e12dbed81
Alecsander <alecsander_marques@hotmail.com>
2018-03-19 20:08:51 -0300

    criada pageadmin e tpl admin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Amcommerce\Page;
+use \Amcommerce\PageAdmin;
 
 $app = new \Slim\Slim();
 
@@ -12,6 +13,14 @@ $app->config('debug', true);
 $app->get('/', function() {
 	
 	$page = new Page();
+
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+	
+	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
