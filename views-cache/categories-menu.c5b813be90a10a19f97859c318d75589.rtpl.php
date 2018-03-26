c208594f5d64af38a567235a3778e15c0ba1ce68
Alecsander <alecsander_marques@hotmail.com>
2018-03-26 14:20:30 -0300

    Site - Categorias

---
+++ views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -0,0 +1,1 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/4">Android</a></li><li><a href="/categories/5">Apple</a></li><li><a href="/categories/8">Asus</a></li><li><a href="/categories/1">Motorola</a></li><li><a href="/categories/6">Samsung</a></li>
