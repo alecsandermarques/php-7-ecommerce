1ca5ac3330d5d38526cf9442945d977e6a2fe915
Alecsander <alecsander_marques@hotmail.com>
2018-03-26 12:16:54 -0300

    admin - categorias

---
+++ vendor/amcommerce/php-classes/src/Model/Category.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Amcommerce\Model;
+
+use \Amcommerce\DB\Sql;
+use \Amcommerce\Model;
+
+class Category extends Model{
+    
+    public static function listAll()
+    {
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_categories ORDER BY descategory");
+    }
+
+    public function save()
+    {
+        $sql = new Sql();
+
+        //na call tem que obedecer a ordem da procedure        
+        $results = $sql->select("CALL sp_categories_save(:idcategory, :descategory )", array(
+            ":idcategory"=>$this->getidcategory(),
+            ":descategory"=>$this->getdescategory()
+        ));
+        
+        $this->setData($results[0]);
+    }
+
+    public function get($idcategory)
+    {
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_categories WHERE idcategory = :idcategory",[
+            ":idcategory"=>$idcategory    
+        ]);
+
+        $this->setData($results[0]);
+    }
+
+    public function delete()
+    {
+        $sql = new Sql();
+
+        $sql->query("DELETE FROM tb_categories WHERE idcategory = :idcategory",[
+            ":idcategory"=>$this->getidcategory()  
+        ]);
+
+    }
+     
+}
+
+
+
+?>
