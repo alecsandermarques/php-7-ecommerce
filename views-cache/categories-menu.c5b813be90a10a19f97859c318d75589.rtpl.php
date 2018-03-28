7a5832b8a6ce81ec0e019370ab2bd9e2ccd9b0ac
Alecsander <alecsander_marques@hotmail.com>
2018-03-28 13:09:21 -0300

    Arrumando Categorias

--- views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -1,1 +1,1 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/4">Android</a></li><li><a href="/categories/5">Apple</a></li><li><a href="/categories/8">Asus</a></li><li><a href="/categories/1">Motorola</a></li><li><a href="/categories/6">Samsung</a></li>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/4">Apple</a></li><li><a href="/categories/3">Asus</a></li><li><a href="/categories/2">Motorola</a></li>
