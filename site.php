add550f193782a25635e7c8d7bced906025b8f19
Alecsander <alecsander_marques@hotmail.com>
2018-03-30 15:01:11 -0300

    site - produtos

--- site.php
+++ site.php
@@ -1,12 +1,17 @@
 <?php
 
-use \Amcommerce\Page;;
+use \Amcommerce\Page;
+use \Amcommerce\Model\Product;
 
 $app->get('/', function() {
+
+	$products = Product::listAll();
 	
 	$page = new Page();
 
-	$page->setTpl("index");
+	$page->setTpl("index",[
+		'products'=>Product::checkList($products)
+	]);
 
 });
 
